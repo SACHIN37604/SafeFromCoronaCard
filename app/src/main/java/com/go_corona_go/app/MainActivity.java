b6a599d3d4d434481f083d75af19699bc84d5248
SACHIN37604 <43314764+SACHIN37604@users.noreply.github.com>
2021-01-06 21:39:12 +0530

    Basic user interface

---
+++ app/src/main/java/com/go_corona_go/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.go_corona_go.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
